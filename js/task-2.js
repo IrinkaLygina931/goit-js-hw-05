e26220c19ba69912b6c298e74c34e80976d3d531
IrinkaLygina931 <114241757+IrinkaLygina931@users.noreply.github.com>
2024-02-26 12:41:28 +0200

    add

--- js/task-2.js
+++ js/task-2.js
@@ -42,53 +42,116 @@
 // console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 // console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
-function calcAverageCalories(days) {
-  let totalCalories = 0;
+// function calcAverageCalories(days) {
+//   let totalCalories = 0;
   
-  days.forEach(day => {
-      totalCalories += day.calories;
-  });
+//   days.forEach(day => {
+//       totalCalories += day.calories;
+//   });
   
-  return totalCalories / days.length;
+//   return totalCalories / days.length;
+// }
+
+// // Перевірка роботи функції
+
+// const daysData = [
+//   { day: 'Monday', calories: 2000 },
+//   { day: 'Tuesday', calories: 1800 },
+//   { day: 'Wednesday', calories: 2200 },
+//   { day: 'Thursday', calories: 2400 },
+//   { day: 'Friday', calories: 1900 },
+//   { day: 'Saturday', calories: 2100 },
+//   { day: 'Sunday', calories: 2300 }
+// ];
+
+// console.log(calcAverageCalories(daysData)); // Виведе середньодобове значення калорій
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 3010 },
+//     { day: "tuesday", calories: 3200 },
+//     { day: "wednesday", calories: 3120 },
+//     { day: "thursday", calories: 2900 },
+//     { day: "friday", calories: 3450 },
+//     { day: "saturday", calories: 3280 },
+//     { day: "sunday", calories: 3300 }
+//   ])
+// ); // 3180
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 2040 },
+//     { day: "tuesday", calories: 2270 },
+//     { day: "wednesday", calories: 2420 },
+//     { day: "thursday", calories: 1900 },
+//     { day: "friday", calories: 2370 },
+//     { day: "saturday", calories: 2280 },
+//     { day: "sunday", calories: 2610 }
+//   ])
+// ); // 2270
+
+// console.log(
+//   calcAverageCalories([])
+// ); // 0
+
+
+
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter(user => user.friends.includes(friendName));
 }
 
-// Перевірка роботи функції
-
-const daysData = [
-  { day: 'Monday', calories: 2000 },
-  { day: 'Tuesday', calories: 1800 },
-  { day: 'Wednesday', calories: 2200 },
-  { day: 'Thursday', calories: 2400 },
-  { day: 'Friday', calories: 1900 },
-  { day: 'Saturday', calories: 2100 },
-  { day: 'Sunday', calories: 2300 }
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
 ];
 
-console.log(calcAverageCalories(daysData)); // Виведе середньодобове значення калорій
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
-console.log(
-  calcAverageCalories([])
-); // 0
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
